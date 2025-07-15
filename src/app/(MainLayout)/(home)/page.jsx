40fbb94c05190ebb318457fee15f747405c346e1
rayhan108 <raihanshorker56@gmail.com>
2025-07-15 11:47:35 +0600

    added dashboard page design

--- src/app/(MainLayout)/(home)/page.jsx
+++ src/app/(MainLayout)/(home)/page.jsx
@@ -6,24 +6,24 @@ import React from 'react';
 import { IoMdPlay } from 'react-icons/io';
 
 const HomePage = () => {
-    return (
-        <div>
-        <Banner/>
-        <Stats/>
-        <Features/>
-        <Join/>
-              {/* Feedback Button */}
-<div className="fixed top-96 right-4">
-  <button className="bg-[#679046] hover:bg-green-700 text-white px-4 py-2 md:py-3 rounded-md text-sm font-medium w-[100px] md:w-auto relative z-10">
-    Feedback!!!
-  </button>
-  <IoMdPlay
-    size={40}              
-    className="text-[#679046] rotate-180 absolute top-4 md:top-6 right-4 z-0"
-  />
-</div>
-        </div>
-    );
+  return (
+    <div>
+      <Banner />
+      <Stats />
+      <Features />
+      <Join />
+      {/* Feedback Button */}
+      <div className="fixed top-96 right-4">
+        <button className="bg-[#679046]  text-white px-4 py-2 md:py-3 rounded-md text-sm font-medium w-[100px] md:w-auto relative z-10">
+          Feedback!!!
+        </button>
+        <IoMdPlay
+          size={40}
+          className="text-[#679046] rotate-180 absolute top-4 md:top-6 right-4 z-0"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
